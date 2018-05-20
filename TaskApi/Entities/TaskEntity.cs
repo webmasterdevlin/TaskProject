210db98dda1d5840898acd1bad07683fb63c96d3
webmasterdevlin <webmasterdevlin@gmail.com>
2018-05-21 01:34:09 +0800

    Added required annotations in TaskEntity

--- TaskApi/Entities/TaskEntity.cs
+++ TaskApi/Entities/TaskEntity.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskApi.Entities
 {
     public class TaskEntity
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [Required]
         public bool IsDone { get; set; }
         public DateTime DeadLine { get; set; }
     }
---
+++ TaskApi/Migrations/20180520173015_AddedAnnotationsInEntities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskApi.Migrations
+{
+    public partial class AddedAnnotationsInEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskEntity",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskEntity",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
