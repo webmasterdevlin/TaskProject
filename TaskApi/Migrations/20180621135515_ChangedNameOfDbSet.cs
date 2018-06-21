330f2497c2b00fc04be44d4411ed4460e900ac1e
webmasterdevlin <webmasterdevlin@gmail.com>
2018-06-21 22:16:17 +0800

    db migration added

---
+++ TaskApi/Migrations/20180621135515_ChangedNameOfDbSet.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskApi.Migrations
+{
+    public partial class ChangedNameOfDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskEntity",
+                table: "TaskEntity");
+
+            migrationBuilder.RenameTable(
+                name: "TaskEntity",
+                newName: "Tasks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "TaskEntity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskEntity",
+                table: "TaskEntity",
+                column: "Id");
+        }
+    }
+}
